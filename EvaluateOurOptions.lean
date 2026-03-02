0bcb9959179036e1a0f1e8ceb9396552413af13e
normalifelias <61462669+normalifelias@users.noreply.github.com>
2026-03-02 15:22:26 +0100

    Fixes for the fixes

--- EvaluateOurOptions.lean
+++ EvaluateOurOptions.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `EvaluteOurOptions` library.
+-- This module serves as the root of the `EvaluateOurOptions` library.
 -- Import modules here that should be built as part of the library.
-import EvaluteOurOptions.Basic
+import EvaluateOurOptions.Basic
