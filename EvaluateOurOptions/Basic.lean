1c43e49546a024ccea4b2e7229900977460f5243
normalifelias <61462669+normalifelias@users.noreply.github.com>
2026-03-05 20:06:13 +0100

    Minor improvements + ToDo list

--- EvaluateOurOptions/Basic.lean
+++ EvaluateOurOptions/Basic.lean
@@ -95,11 +95,19 @@ partial def quicksortHelper [Ord α] (todos : List (Todo α)) (acc : Array α) :
       | some (hd, #[]) => quicksortHelper rest (acc.push hd)
       | some (hd, tl) =>
         let (lt, gt) := pivotsplit tl hd
-        let newtodos := [Todo.Sort lt, Todo.Push hd, Todo.Sort gt] ++ rest
-        quicksortHelper newtodos acc
+        let new := Todo.Sort lt :: Todo.Push hd :: Todo.Sort gt :: rest
+        quicksortHelper new acc
 
 def quicksort [Ord α] (arr : Array α) : Array α :=
   quicksortHelper [Todo.Sort arr] #[]
 
 def demoArray : Array Nat := #[47, 13, 82, 6, 91, 34, 57, 23, 76, 41, 88, 3, 65, 29, 54, 17, 72, 39, 84, 11, 63, 28, 95, 42, 7, 56, 31, 78, 19, 67, 44, 90, 25, 58, 14, 83, 37, 62, 9, 71, 48, 26, 93, 15, 52, 38, 77, 22, 69, 4, 86, 33, 61, 18, 45, 79, 12, 57, 35, 81, 24, 68, 43, 96, 8, 53, 27, 74, 16, 89, 41, 64, 30, 55, 20, 73, 46, 85, 10, 60, 36, 92, 21, 49, 66, 32, 75, 5, 87, 40, 59, 28, 70, 38, 94, 50, 80, 2, 97, 44]
 #eval quicksort demoArray
+def demoArray2 : Array String := #["Byte", "Gamma", "%", "Alpha", "·", "Beta", "Uranium", "$", "Aaron", "Xenon", "G", "e", "f(x)", "Über", "×"]
+#eval quicksort demoArray2
+
+-- TODO:
+-- · Smart pivot selection / Alternate algorithm
+-- · Commentary for future reference
+-- · Prove termination
+-- · Prove sorting
